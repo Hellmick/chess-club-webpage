1e94e50229cb3566fe700301418369ba9f145960
Kalinowski <35411861+Hellmick@users.noreply.github.com>
2023-02-19 00:18:13 +0100

    Add files via upload

---
+++ igor/script.js
@@ -0,0 +1,25 @@
+var image = 1;
+const maxImage = 4;
+
+function slider() {
+    setInterval(function() {
+        let source = "url('./img/slider/image" + image.toString() + ".png')";
+        document.getElementById("sliderImage").style.backgroundImage = source;
+        image++;
+        if (image > maxImage) image = 1;
+    }, "5000");
+}
+
+function sliderNext() {
+    image++;
+    if (image > maxImage) image = 1;
+    let source = "url('./img/slider/image" + image.toString() + ".png')";
+    document.getElementById("sliderImage").style.backgroundImage = source;
+}
+
+function sliderPrevious() {
+    image--;
+    if (image < 1) image = maxImage;
+    let source = "url('./img/slider/image" + image.toString() + ".png')";
+    document.getElementById("sliderImage").style.backgroundImage = source;
+}
